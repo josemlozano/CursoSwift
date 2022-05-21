ddf644d7d24110ba353aebd43b2338a60b427248
josemlozano <josemlozano6@gmail.com>
2022-05-21 18:24:31 +0200

    Commit inicial

---
+++ CursoSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CursoSwift
+//
+//  Created by Jose Muñoz on 21/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CursoSwift/CursoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoSwiftApp.swift
+//  CursoSwift
+//
+//  Created by Jose Muñoz on 21/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CursoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
