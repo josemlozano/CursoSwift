3dd0f5d55b6fb642ca80466e91623e2fc182d0bf
josemlozano <josemlozano6@gmail.com>
2022-05-21 18:30:25 +0200

    cambio a storyboard

---
+++ CursoSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CursoSwift
+//
+//  Created by Jose Muñoz on 21/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
