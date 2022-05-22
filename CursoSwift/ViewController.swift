b6200e678493f61c51542ad710a61da8751ecfb7
josemlozano <josemlozano6@gmail.com>
2022-05-22 13:32:11 +0200

    auto layout inicio

--- CursoSwift/ViewController.swift
+++ CursoSwift/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var etiqueta: UILabel!
+    @IBOutlet weak var btCambiarTexto: UIButton!
+    var isTrue = false
+    var n = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cambiarTexto(_ sender: Any) {
+        if n >= 20 {
+            etiqueta.text = "PUTAAAAAAAA"
+        } else if (isTrue) {
+            etiqueta.text="Eres"
+            isTrue = !isTrue
+        } else {
+            etiqueta.text="Puta"
+            isTrue = !isTrue
+        }
+        n += 1
+    }
+    
 }
 
